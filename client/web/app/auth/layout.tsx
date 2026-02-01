aa4975aa7ada5524b470fd263db06ed11af7c9cb
tay52636 <phuongtay52636@gmail.com>
2026-02-01 09:08:31 +0700

    feat(web): new header , code page

---
+++ client/web/app/auth/layout.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: " Saigon cuisine hub | Auth",
+    description: "Authentication pages",
+};
+
+export default function AuthLayout({
+    children,
+}: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <div className="min-h-screen bg-background">
+            {children}
+        </div>
+    );
+}
---
+++ client/web/components/features/Home/HomePages.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from 'react'
+
+export default function HomePages() {
+    return (
+        <div>HomePages</div>
+    )
+}
